b45c7c091d76968b2102bc54f0837cde426734d8
sudo-gera <gera.tatarinov.999@gmail.com>
2019-12-04 08:09:05 +0300

    send

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ if token==null:
 скопируйте url в поле ниже
 <form>
 <input id="reg_token" type="text">
+<input onckick="reg_token_h" label="enter" type="button">
 </form>
 <div id="debug"></div>
 ''',"main")
@@ -27,7 +28,7 @@ def reg_token_h():
   oth=token.indexOf('&',acc)
   token=token.slice(acc+13,oth)
   alert(token)
- */
 reg_token.onclick=reg_token_h()
+ */
 </script>
 </body>
